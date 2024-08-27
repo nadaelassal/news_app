33ce2e30fdac538d9c1f31c594d1b9b481b052bb
SERVER\portal2 <nadaelassal00@icloud.com>
2024-08-27 11:25:13 +0200

    inatialze project

---
+++ lib/main.dart
@@ -0,0 +1,22 @@
+// ignore_for_file: prefer_const_literals_to_create_immutables, prefer_const_constructors
+
+import 'package:flutter/material.dart';
+import 'package:news_app/home_screen.dart';
+
+void main() {
+  runApp(const NewsApp());
+}
+
+class NewsApp extends StatelessWidget {
+  const NewsApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      routes: {
+        HomeScreen.routeName: (_) => HomeScreen(),
+      },
+    );
+  }
+}
