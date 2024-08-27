a96b3f17cdc273e1b78cff42bf2467120cdc7cce
SERVER\portal2 <nadaelassal00@icloud.com>
2024-08-27 13:11:45 +0200

    categories

---
+++ lib/app_theme.dart
@@ -0,0 +1,32 @@
+// ignore_for_file: prefer_const_constructors
+
+import 'package:flutter/material.dart';
+
+class AppTheme {
+  static const Color primary = Color(0XFF39A552);
+  static const Color navy = Color(0XFF4F5A69);
+  static const Color black = Color(0XFF303030);
+  static const Color white = Color(0XFFFFFFFF);
+
+  static ThemeData lightTheme = ThemeData(
+    primaryColor: primary,
+    appBarTheme: AppBarTheme(
+      backgroundColor: primary,
+      foregroundColor: white,
+      titleTextStyle:
+          TextStyle(color: white, fontSize: 22, fontWeight: FontWeight.w400),
+      centerTitle: true,
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.horizontal(
+          left: Radius.circular(32),
+          right: Radius.circular(32),
+        ),
+      ),
+    ),
+    scaffoldBackgroundColor: Colors.transparent,
+    textTheme: TextTheme(
+      titleLarge:
+          TextStyle(fontSize: 22, fontWeight: FontWeight.bold, color: white),
+    ),
+  );
+}
